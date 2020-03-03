9f7cfcee2e58fdd08ef98019139aeec59d14405d
Illia Polliul <1294882+ilush@users.noreply.github.com>
2020-03-04 00:09:39 +0100

    HW1 is completed

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
 func main() {
-	// Place your code here
+	time_n, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("current time:", time.Now())
+	fmt.Println("exact time:", time_n)
+
 }
