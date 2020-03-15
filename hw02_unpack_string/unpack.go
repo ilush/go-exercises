08ad8ae1adda399e35445bef8dd0b50a8c2c98dd
Illia Polliul <1294882+ilush@users.noreply.github.com>
2020-03-15 23:05:49 +0100

    hw02 done

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -1,12 +1,35 @@
-package hw02_unpack_string //nolint:golint,stylecheck
+package hw02_unpack_string //nolint:golint,stylecheck,gomnd
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here
-	return "", nil
+func Unpack(s string) (string, error) {
+	var runeBuf rune
+	var output string
+
+	for _, char := range s {
+		if !unicode.IsDigit(char) {
+			output += string(char)
+			if char == runeBuf {
+				// repeated characters
+				return "", ErrInvalidString
+			}
+			runeBuf = char
+		} else {
+			count, _ := strconv.Atoi(string(char))
+			output += strings.Repeat(string(runeBuf), count-1)
+			if runeBuf == 0 || unicode.IsDigit(runeBuf) {
+				// sequential numbers or string starting with number
+				return "", ErrInvalidString
+			}
+			runeBuf = char // for sequential numbers check
+		}
+	}
+	return output, nil
 }
