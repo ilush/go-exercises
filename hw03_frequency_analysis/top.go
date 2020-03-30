f6dc97075dd5559d45cd929f6a9ef7a8d2187652
Illia Polliul <1294882+ilush@users.noreply.github.com>
2020-03-30 12:20:49 +0200

    hw03 semi-ready

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,39 @@
 package hw03_frequency_analysis //nolint:golint,stylecheck
 
-func Top10(_ string) []string {
-	// Place your code here
-	return nil
+import (
+	"regexp"
+	"sort"
+	"strings"
+)
+
+func Top10(in string) []string {
+	in = regexp.MustCompile(`\n`).ReplaceAllString(in, " ")
+	in = regexp.MustCompile(`\t`).ReplaceAllString(in, "")
+	in = regexp.MustCompile(`\s\s+`).ReplaceAllString(in, " ")
+
+	s1 := strings.Split(in, " ")
+	m := make(map[string]int)
+
+	// feed slice into map for counting
+	for _, word := range s1 {
+		count := m[word]
+		m[word] = count + 1
+	}
+
+	// create new slice out of unique words
+	s2 := make([]string, 0, len(m))
+	for key := range m {
+		s2 = append(s2, key)
+	}
+
+	// sort based on map values
+	sort.Slice(s2, func(i, j int) bool { return m[s2[i]] > m[s2[j]] })
+
+	// return slice up to 10 elements
+	top := 10
+	if len(m) <= top {
+		return s2
+	} else {
+		return s2[:top]
+	}
 }
